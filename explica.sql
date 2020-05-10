7acf9336ec8185b80c91783214f77ea0a905e75a
juanfelipecp <53978290+juanfelipecp@users.noreply.github.com>
2020-05-10 18:16:41 -0500

    Update explica.sql
    sisa

--- explica.sql
+++ explica.sql
@@ -16,74 +16,77 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`explica` /*!40100 DEFAULT CHARACTER SET
 
 USE `explica`;
 
-/*Table structure for table `albunes` */
+/*Table structure for table `albumes` */
 
-DROP TABLE IF EXISTS `albunes`;
+DROP TABLE IF EXISTS `albumes`;
 
-CREATE TABLE `albunes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `albumes` (
+  `id_album` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `artista` varchar(100) DEFAULT NULL,
   `fecha` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+  `link` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_album`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `albunes` */
+/*Data for the table `albumes` */
 
-insert  into `albunes`(`id`,`nombre`,`artista`,`fecha`) values 
-(1,'hold_yout_color','pendulum','2005-08-07'),
-(2,'in_silico','pendulum','2008-02-08'),
-(3,'inmersions','pendulum','2010-01-05');
+insert  into `albumes`(`id_album`,`nombre`,`artista`,`fecha`,`link`) values 
+(1,'hold_yout_colour','pendulum','2005-08-07','https://mega.nz/#!EEghzRQA!yiyFm2w7WfpkF7hMMcyhdwu9tCVFGY6HpIuBesNs7Mo'),
+(11,'slipknot','slipknot','0000-00-00','http://www.mediafire.com/file/26y58ubkm1t6ku6/%25281999%2529_Slipknot.rar/file'),
+(12,'awsome mix vol 1 y 2','madre con calvicie','2012-05-12','https://mega.nz/file/xZQQkajb#AS0oklppDZLw3RU1c2_ZoX8-azfhgXSsQbv7LShiU1I');
 
 /*Table structure for table `artista` */
 
 DROP TABLE IF EXISTS `artista`;
 
 CREATE TABLE `artista` (
-  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `id_artista` int(100) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `genero` varchar(100) DEFAULT NULL,
   `albumes` int(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id_artista`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `artista` */
 
-insert  into `artista`(`id`,`nombre`,`genero`,`albumes`) values 
-(1,'pendulum','drum_and_bass',3);
+insert  into `artista`(`id_artista`,`nombre`,`genero`,`albumes`) values 
+(1,'pendulum','drum_and_bass',3),
+(3,'slipknot','nu metal',6),
+(8,'madre con calvicie','variado',2);
 
-/*Table structure for table `roles` */
+/*Table structure for table `peticiones` */
 
-DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `peticiones`;
 
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rol_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `peticiones` (
+  `id_peticiones` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `genero` varchar(100) DEFAULT NULL,
+  `album` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_peticiones`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `roles` */
+/*Data for the table `peticiones` */
+
+insert  into `peticiones`(`id_peticiones`,`nombre`,`genero`,`album`) values 
+(27,'radiohead','rock','ok computer');
 
 /*Table structure for table `usuarios` */
 
 DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usua` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(100) DEFAULT NULL,
   `contra` varchar(100) DEFAULT NULL,
-  `rol_id` int(11) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `edad` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3334 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id_usua`)
+) ENGINE=InnoDB AUTO_INCREMENT=3335 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `usuarios` */
 
-insert  into `usuarios`(`id`,`usuario`,`contra`,`rol_id`,`nombre`,`edad`) values 
-(4,'leon','8888',1,'leo',38),
-(5,'cp','7777',1,'felipe',80),
-(6,'lolsito','9999',1,'sebastian',19);
+insert  into `usuarios`(`id_usua`,`usuario`,`contra`) values 
+(1,'admin','123');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
